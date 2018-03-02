2eccabd652702d3c0ed573a49ceb3e1c229ab676
Doug Schrashun <doug@cnevids.com>
2018-03-02 16:24:03 -0500

    test cssProcessor call

--- test/webpack-integration.test.js
+++ test/webpack-integration.test.js
@@ -2,6 +2,8 @@
 import fs from 'fs';
 import path from 'path';
 import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import OptimizeCssAssetsPlugin from '../';
 
 const cases = process.env.CASES ? process.env.CASES.split(',') : fs.readdirSync(path.join(__dirname, 'cases'));
 
@@ -38,6 +40,32 @@ describe('Webpack Integration Tests', () => {
       });
     });
   });
+
+  it('calls cssProcessor with correct arguments', (done) => {
+    fs.readFile(__dirname + '/default/default.css', (err, data) => {
+      const expectedCss = data.toString();
+      const destination = 'tmp.css';
+      const cssProcessor = {
+        process: (css, options) => {
+          const assembledOptions = Object.assign({ from: destination, to: destination }, options);
+          expect(options).toEqual(assembledOptions);
+          expect(css).toEqual(expectedCss);
+          return Promise.resolve({ css });
+        }
+      };
+      const cssProcessorOptions = { discardComments: { removeAll: true } };
+      const plugin = new OptimizeCssAssetsPlugin({
+        cssProcessor,
+        cssProcessorOptions
+      });
+      const config = Object.assign(defaultConfig, {plugins: [plugin, new ExtractTextPlugin(destination)]});
+      webpack(config, (err, stats) => {
+        if (err) return done(err);
+        if (stats.hasErrors()) return done(new Error(stats.toString()));
+        done();
+      });
+    });
+  });
 });
 
 function readFileOrEmpty(path) {
@@ -45,5 +73,29 @@ function readFileOrEmpty(path) {
     return fs.readFileSync(path, 'utf-8');
   } catch (e) {
     return '';
+  }
+}
+
+const defaultConfig = {
+  entry: './default/index',
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: { loader: 'style-loader' },
+          use: {
+            loader: 'css-loader',
+            options: { minimize: true }
+          }
+        })
+      },
+    ],
+  },
+  plugins: [],
+  context: __dirname,
+  output: {
+    filename: 'tmp.js',
+    path: path.join(__dirname, 'default', 'exports')
   }
 }
